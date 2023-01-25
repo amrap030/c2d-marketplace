d7f2dec6646a24cf28927b63defa20eacb713e9c
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-01-25 23:38:20 +0100

    refactor(compute): remove unnecessary files

--- src/compute-node/src/app.ts
+++ src/compute-node/src/app.ts
@@ -7,76 +7,66 @@ import hpp from "hpp";
 import morgan from "morgan";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from "@config";
+import { PORT, LOG_FORMAT } from "@config";
 import { Routes } from "@interfaces/routes.interface";
 import errorMiddleware from "@middlewares/error.middleware";
 import { logger, stream } from "@utils/logger";
 
-class App {
-  public app: express.Application;
-  public env: string;
-  public port: string | number;
+const app = express();
 
-  constructor(routes: Routes[]) {
-    this.app = express();
-    this.env = NODE_ENV || "development";
-    this.port = PORT || 3000;
+const listen = () => {
+  app.listen(PORT, () => {
+    logger.info(`=================================`);
+    logger.info(`🚀 App listening on the port 3000`);
+    logger.info(`=================================`);
+  });
+};
 
-    this.initializeMiddlewares();
-    this.initializeRoutes(routes);
-    this.initializeSwagger();
-    this.initializeErrorHandling();
-  }
+const initializeMiddlewares = () => {
+  app.use(morgan(LOG_FORMAT, { stream }));
+  app.use(cors({ origin: "*", credentials: true }));
+  app.use(hpp());
+  app.use(helmet());
+  app.use(compression());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+};
 
-  public listen() {
-    this.app.listen(this.port, () => {
-      logger.info(`=================================`);
-      logger.info(`======= ENV: ${this.env} =======`);
-      logger.info(`🚀 App listening on the port ${this.port}`);
-      logger.info(`=================================`);
-    });
-  }
+const initializeRoutes = (routes: Routes[]) => {
+  routes.forEach(route => {
+    app.use("/", route.router);
+  });
+};
 
-  public getServer() {
-    return this.app;
-  }
-
-  private initializeMiddlewares() {
-    this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
-    this.app.use(hpp());
-    this.app.use(helmet());
-    this.app.use(compression());
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(cookieParser());
-  }
-
-  private initializeRoutes(routes: Routes[]) {
-    routes.forEach(route => {
-      this.app.use("/", route.router);
-    });
-  }
-
-  private initializeSwagger() {
-    const options = {
-      swaggerDefinition: {
-        info: {
-          title: "REST API",
-          version: "1.0.0",
-          description: "Example docs",
-        },
+const initializeSwagger = () => {
+  const options = {
+    swaggerDefinition: {
+      info: {
+        title: "REST API",
+        version: "1.0.0",
+        description: "Example docs",
       },
-      apis: ["swagger.yaml"],
-    };
+    },
+    apis: ["swagger.yaml"],
+  };
 
-    const specs = swaggerJSDoc(options);
-    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-  }
+  const specs = swaggerJSDoc(options);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+};
 
-  private initializeErrorHandling() {
-    this.app.use(errorMiddleware);
-  }
-}
+const initializeErrorHandling = () => {
+  app.use(errorMiddleware);
+};
 
-export default App;
+export const startApp = (routes: Routes[]) => {
+  initializeMiddlewares();
+  initializeRoutes(routes);
+  initializeSwagger();
+  initializeErrorHandling();
+  listen();
+};
+
+export const getServer = () => {
+  return app;
+};
--- src/compute-node/src/server.ts
+++ src/compute-node/src/server.ts
@@ -1,11 +1,6 @@
-import App from "@/app";
-import AuthRoute from "@routes/auth.route";
-import IndexRoute from "@routes/index.route";
-import UsersRoute from "@routes/users.route";
-import validateEnv from "@utils/validateEnv";
+import { startApp } from "@/app";
+// import AuthRoute from "@routes/auth.route";
+// import IndexRoute from "@routes/index.route";
+// import UsersRoute from "@routes/users.route";
 
-validateEnv();
-
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
-
-app.listen();
+startApp([]);
