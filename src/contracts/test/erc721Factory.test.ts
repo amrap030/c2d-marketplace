72bc99041eed1cb3564e44263787b9d69d25369c
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-02-18 18:42:46 +0100

    test(contracts): add missing tests for marketplace

--- src/contracts/test/erc721Factory.test.ts
+++ src/contracts/test/erc721Factory.test.ts
@@ -6,15 +6,18 @@ import { INTERFACES } from "../utils/constants";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { makeInterfaceId } = require("@openzeppelin/test-helpers");
 
-export let newErc721Token: ERC721Template;
+export let datasetToken: ERC721Template;
+export let algorithmTokenOne: ERC721Template;
+export let algorithmTokenTwo: ERC721Template;
 
 describe("ERC721Factory", async () => {
   let erc721Factory: ERC721Factory,
     erc721Template: ERC721Template,
-    owner: SignerWithAddress;
+    sender: SignerWithAddress,
+    developer: SignerWithAddress;
 
   before(async function() {
-    [owner] = await ethers.getSigners();
+    [, , sender, developer] = await ethers.getSigners();
 
     const ERC721Template = await ethers.getContractFactory("ERC721Template");
     const ERC721Factory = await ethers.getContractFactory("ERC721Factory");
@@ -26,11 +29,11 @@ describe("ERC721Factory", async () => {
     await erc721Factory.deployed();
   });
 
-  it("create dataset and mint token for owner", async () => {
+  it("create dataset and mint token for sender", async () => {
     const tx = await erc721Factory.cloneContract(
-      owner.address,
-      "C2D",
-      "C2DYMBOL",
+      sender.address,
+      "GODDOG-95",
+      "Godly Dogma Token",
       "ipfs://bafkreihuk7qqpdskouyjax5wzpn4wwaktquzjd3flbluz6wbv46m7kt74u/",
       0,
     );
@@ -43,7 +46,7 @@ describe("ERC721Factory", async () => {
       );
       const newTokenAddress = (event?.args as string[])[3] as string;
 
-      newErc721Token = await ethers.getContractAt(
+      datasetToken = await ethers.getContractAt(
         "ERC721Template",
         newTokenAddress,
       );
@@ -52,14 +55,90 @@ describe("ERC721Factory", async () => {
     }
   });
 
-  it("balance of owner equals 1", async () => {
-    expect(await newErc721Token.balanceOf(owner.address)).to.equal(1);
+  it("balance of sender after mint eq 1", async () => {
+    expect(await datasetToken.balanceOf(sender.address)).to.equal(1);
   });
 
-  it("new contract supports all interfaces", async () => {
+  it("dataset supports all interfaces", async () => {
     for (const k of ["ERC165", "ERC721", "ERC721Metadata"]) {
       const interfaceId = makeInterfaceId.ERC165(INTERFACES[k]);
-      expect(await newErc721Token.supportsInterface(interfaceId)).to.equal(
+      expect(await datasetToken.supportsInterface(interfaceId)).to.equal(true);
+    }
+  });
+
+  it("create algorithm and mint token for developer", async () => {
+    const tx = await erc721Factory.cloneContract(
+      developer.address,
+      "SPINOS-67",
+      "Spiky Nose Token",
+      "ipfs://bafkreihuk7qqpdskouyjax5wzpn4wwaktquzjd3flbluz6wbv46m7kt74u/",
+      1,
+    );
+
+    const txReceipt = await tx.wait();
+
+    if (txReceipt.events) {
+      const event = txReceipt.events.find(
+        event => event.event === "ERC721Created",
+      );
+      const newTokenAddress = (event?.args as string[])[3] as string;
+
+      algorithmTokenOne = await ethers.getContractAt(
+        "ERC721Template",
+        newTokenAddress,
+      );
+    } else {
+      throw "Transaction failed!";
+    }
+  });
+
+  it("balance of developer after mint eq 1", async () => {
+    expect(await algorithmTokenOne.balanceOf(developer.address)).to.equal(1);
+  });
+
+  it("algorithm supports all interfaces", async () => {
+    for (const k of ["ERC165", "ERC721", "ERC721Metadata"]) {
+      const interfaceId = makeInterfaceId.ERC165(INTERFACES[k]);
+      expect(await algorithmTokenOne.supportsInterface(interfaceId)).to.equal(
+        true,
+      );
+    }
+  });
+
+  it("create second algorithm and mint token for developer", async () => {
+    const tx = await erc721Factory.cloneContract(
+      developer.address,
+      "SPITEE-33",
+      "Spiky Teeth Token",
+      "ipfs://bafkreihuk7qqpdskouyjax5wzpn4wwaktquzjd3flbluz6wbv46m7kt74u/",
+      1,
+    );
+
+    const txReceipt = await tx.wait();
+
+    if (txReceipt.events) {
+      const event = txReceipt.events.find(
+        event => event.event === "ERC721Created",
+      );
+      const newTokenAddress = (event?.args as string[])[3] as string;
+
+      algorithmTokenTwo = await ethers.getContractAt(
+        "ERC721Template",
+        newTokenAddress,
+      );
+    } else {
+      throw "Transaction failed!";
+    }
+  });
+
+  it("balance of developer after mint eq 1", async () => {
+    expect(await algorithmTokenTwo.balanceOf(developer.address)).to.equal(1);
+  });
+
+  it("algorithm supports all interfaces", async () => {
+    for (const k of ["ERC165", "ERC721", "ERC721Metadata"]) {
+      const interfaceId = makeInterfaceId.ERC165(INTERFACES[k]);
+      expect(await algorithmTokenTwo.supportsInterface(interfaceId)).to.equal(
         true,
       );
     }
--- src/contracts/test/marketplace.test.ts
+++ src/contracts/test/marketplace.test.ts
@@ -1,19 +1,74 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { Marketplace, Verifier } from "../typechain";
 import proof from "../contracts/mock/proof.json";
 import { BigNumber } from "ethers";
-import { newErc721Token } from "./erc721Factory.test";
+import {
+  datasetToken,
+  algorithmTokenOne,
+  algorithmTokenTwo,
+} from "./erc721Factory.test";
+
+const n = 2;
+const length = 32;
+const depth = 2;
+
+const keyCommit =
+  "0x4833fd0df81fe66248fa7a7a858ace1eb6cda67d46db68de29d210f9811fbac3";
+const key =
+  "0x9eec88b55d9295bb2ac6d35562a2ca5cebfe4e64feb234c2a8dde95d1b6cd2a2";
+const fakeKeyCommit =
+  "0xc6c6fd4c742e3f2b985047de939c1045901b5fc445f4436e5322623c001eebdc";
+const fakeKey =
+  "0xddc7b15845dcabf17870858845a4e5c3f091f8dc831fd04503abb2331fb7f376";
+
+const cipherTextRoot =
+  "0xaabc52bbd65f0df6af50c828af1c299d0406898d34ad08b270e31eb4769cd4ec";
+const plainDataRoot =
+  "0xb1ea444fe88a2ab4f6add48eda24ce89497ced4dee1bf978f818853f35252939";
+
+const complainRootData = {
+  correctLeaf:
+    "0x84e68d3d42bf62961ab1a3ca38631ba089c4178aa0eab5c355f4ee761dcd6e6a",
+  correctProof: [
+    "0x5464455ff0684b4442b81a53889452ca094534f8c92dec57b4980d49d20ecba6",
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
+  ],
+};
+
+const prices = [5, 10];
+
+const pkAddress =
+  "ipfs://bafkreihuk7qqpdskouyjax5wzpn4wwaktquzjd3flbluz6wbv46m7kt74u/";
+
+const complainLeafData = {
+  indexOut: 2,
+  indexIn: 0,
+  Zout: "0x84e68d3d42bf62961ab1a3ca38631ba089c4178aa0eab5c355f4ee761dcd6e6a",
+  Zin1: ["0xa82f4ee331bcebaf0a215a715903c89194aba5870af05651db498e58420c5bf0"],
+  Zin2: ["0xbabfac87c72b527d9ea485bf5c47c4f6a0804bb0deee2c857bb58df735f9967f"],
+  proofZout: [
+    "0x5464455ff0684b4442b81a53889452ca094534f8c92dec57b4980d49d20ecba6",
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
+  ],
+  proofZin: [
+    "0x12c4515305ae87d82f12d90d2a3c07698971a9aa8ccfea2e0cfd5661fcd0e124",
+    "0xbd31efa3dd2994356e6e57ed601db2938a369fb6f27e0577fd07a78aac509414",
+  ],
+};
 
 describe("Marketplace", async () => {
   let marketplace: Marketplace,
     verifier: Verifier,
-    buyer: SignerWithAddress,
-    seller: SignerWithAddress;
+    receiver: SignerWithAddress,
+    sender: SignerWithAddress,
+    firstSessionId: string,
+    secondSessionId: string;
 
   before(async function() {
-    [, buyer, seller] = await ethers.getSigners();
+    [, receiver, sender] = await ethers.getSigners();
 
     const Marketplace = await ethers.getContractFactory("Marketplace");
     const Verifier = await ethers.getContractFactory("Verifier");
@@ -25,73 +80,302 @@ describe("Marketplace", async () => {
     await verifier.deployed();
   });
 
-  it("list two items", async () => {
+  it("create two offerings", async () => {
     await expect(
       marketplace
-        .connect(seller)
-        .createItem(
-          newErc721Token.address,
-          [
-            "0xB7c7653c6eDd43d482704f0A7C8e7bc04c367f48",
-            "0x798c72E326BF7A7d848caeD892DCae9C233D27Ec",
-          ],
-          [5, 10],
+        .connect(sender)
+        .createOffer(
+          datasetToken.address,
+          [algorithmTokenOne.address, algorithmTokenTwo.address],
+          prices,
         ),
     )
-      .to.emit(marketplace, "ItemCreated")
+      .to.emit(marketplace, "OfferCreated")
       .withArgs(
-        newErc721Token.address,
-        "0xB7c7653c6eDd43d482704f0A7C8e7bc04c367f48",
-        [seller.address, 5],
+        datasetToken.address,
+        algorithmTokenOne.address,
+        sender.address,
+        prices[0],
       )
-      .to.emit(marketplace, "ItemCreated")
+      .to.emit(marketplace, "OfferCreated")
       .withArgs(
-        newErc721Token.address,
-        "0x798c72E326BF7A7d848caeD892DCae9C233D27Ec",
-        [seller.address, 10],
+        datasetToken.address,
+        algorithmTokenTwo.address,
+        sender.address,
+        prices[1],
       );
   });
 
-  it("create order", async () => {
+  it("create first order", async () => {
+    const oneHour = 60 * 60;
+    firstSessionId = ethers.utils.soliditySha256(
+      ["address", "address", "address"],
+      [sender.address, receiver.address, algorithmTokenOne.address],
+    );
+
+    const currentBlock = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(currentBlock);
+
     await expect(
       marketplace
-        .connect(buyer)
+        .connect(receiver)
         .createOrder(
-          newErc721Token.address,
+          datasetToken.address,
           verifier.address,
-          "0xB7c7653c6eDd43d482704f0A7C8e7bc04c367f48",
-          "ipfs://bafkreihuk7qqpdskouyjax5wzpn4wwaktquzjd3flbluz6wbv46m7kt74u/",
-          {
-            value: BigNumber.from(5),
-          },
+          algorithmTokenOne.address,
+          pkAddress,
         ),
     )
       .to.emit(marketplace, "OrderCreated")
       .withArgs(
-        seller.address,
-        "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-        newErc721Token.address,
-        "ipfs://bafkreihuk7qqpdskouyjax5wzpn4wwaktquzjd3flbluz6wbv46m7kt74u/",
-        5,
+        0,
+        sender.address,
+        receiver.address,
+        datasetToken.address,
+        algorithmTokenOne.address,
+        verifier.address,
+        pkAddress,
+        block.timestamp + (oneHour + 1),
+        oneHour,
+        prices[0],
+        firstSessionId,
+      );
+
+    expect(await marketplace.provider.getBalance(marketplace.address)).equal(0);
+  });
+
+  it("failed proof of computation with fake data", async () => {
+    await expect(
+      marketplace.connect(sender).proofComputation(
+        firstSessionId,
+        depth,
+        length,
+        n,
+        keyCommit,
+        cipherTextRoot,
+        plainDataRoot,
+        [proof.inputs[0].replace(/c/g, "b"), ...proof.inputs.slice(1)],
+        proof.proof as any, // eslint-disable-line
+      ),
+    ).to.be.reverted;
+  });
+
+  it("successful proof of computation with correct data", async () => {
+    await expect(
+      marketplace.connect(sender).proofComputation(
+        firstSessionId,
+        depth,
+        length,
+        n,
+        keyCommit,
+        cipherTextRoot,
+        plainDataRoot,
+        proof.inputs,
+        proof.proof as any, // eslint-disable-line
+      ),
+    )
+      .to.emit(marketplace, "OrderInitialized")
+      .withArgs(
+        depth,
+        length,
+        n,
+        keyCommit,
+        cipherTextRoot,
+        plainDataRoot,
+        firstSessionId,
+      );
+  });
+
+  it("pay for first order", async () => {
+    await expect(
+      marketplace.connect(receiver).buy(firstSessionId, {
+        value: BigNumber.from(5),
+      }),
+    )
+      .to.emit(marketplace, "OrderAccepted")
+      .withArgs(firstSessionId);
+
+    expect(await marketplace.provider.getBalance(marketplace.address)).equal(5);
+  });
+
+  it("reveal correct key for first order", async () => {
+    await expect(marketplace.connect(sender).reveal(firstSessionId, key))
+      .to.emit(marketplace, "OrderRevealed")
+      .withArgs(key, firstSessionId);
+  });
+
+  it("failed complain about root with correct data because sender was honest", async () => {
+    const tx = await marketplace
+      .connect(receiver)
+      .complainAboutRoot(
+        firstSessionId,
+        complainRootData.correctLeaf,
+        complainRootData.correctProof,
+      );
+
+    await tx.wait();
+
+    expect(await marketplace.provider.getBalance(marketplace.address)).equal(5);
+  });
+
+  it("failed complain about root with fake data from receiver", async () => {
+    const fakeLeaf = complainRootData.correctLeaf.replace(/c/g, "b");
+
+    await expect(
+      marketplace
+        .connect(receiver)
+        .complainAboutRoot(
+          firstSessionId,
+          fakeLeaf,
+          complainRootData.correctProof,
+        ),
+    ).to.be.revertedWith("Not in encoding");
+  });
+
+  it("failed complain about leaf with fake data from receiver", async () => {
+    await expect(
+      marketplace
+        .connect(receiver)
+        .complainAboutLeaf(
+          firstSessionId,
+          complainLeafData.indexOut,
+          complainLeafData.indexIn,
+          complainLeafData.Zout,
+          complainLeafData.Zin1,
+          complainLeafData.Zin2,
+          complainLeafData.proofZout,
+          complainLeafData.proofZin,
+        ),
+    ).to.be.revertedWith("Not in encoding");
+  });
+
+  it("failed complain about node with fake data from receiver", async () => {
+    await expect(
+      marketplace
+        .connect(receiver)
+        .complainAboutNode(
+          firstSessionId,
+          complainLeafData.indexOut,
+          complainLeafData.indexIn,
+          complainLeafData.Zout,
+          complainLeafData.Zin1[0],
+          complainLeafData.Zin2[0],
+          complainLeafData.proofZout,
+          complainLeafData.proofZin,
+        ),
+    ).to.be.revertedWith("Not in encoding");
+  });
+
+  it("create second order", async () => {
+    const oneHour = 60 * 60;
+    secondSessionId = ethers.utils.soliditySha256(
+      ["address", "address", "address"],
+      [sender.address, receiver.address, algorithmTokenTwo.address],
+    );
+
+    const currentBlock = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(currentBlock);
+
+    await expect(
+      marketplace
+        .connect(receiver)
+        .createOrder(
+          datasetToken.address,
+          verifier.address,
+          algorithmTokenTwo.address,
+          pkAddress,
+        ),
+    )
+      .to.emit(marketplace, "OrderCreated")
+      .withArgs(
+        0,
+        sender.address,
+        receiver.address,
+        datasetToken.address,
+        algorithmTokenTwo.address,
+        verifier.address,
+        pkAddress,
+        block.timestamp + (oneHour + 1),
+        oneHour,
+        prices[1],
+        secondSessionId,
       );
 
     expect(await marketplace.provider.getBalance(marketplace.address)).equal(5);
   });
 
-  it("verify computation", async () => {
-    const sellerBalance = await marketplace.provider.getBalance(seller.address);
+  it("successful proof of computation but fake key commit submitted", async () => {
+    await expect(
+      marketplace.connect(sender).proofComputation(
+        secondSessionId,
+        depth,
+        length,
+        n,
+        fakeKeyCommit,
+        cipherTextRoot,
+        plainDataRoot,
+        proof.inputs,
+        proof.proof as any, // eslint-disable-line
+      ),
+    )
+      .to.emit(marketplace, "OrderInitialized")
+      .withArgs(
+        depth,
+        length,
+        n,
+        fakeKeyCommit,
+        cipherTextRoot,
+        plainDataRoot,
+        secondSessionId,
+      );
+  });
 
-    const tx = await marketplace.connect(buyer).verifyComputation(
-      newErc721Token.address,
-      0,
-      proof.inputs,
-      proof.proof as any, // eslint-disable-line
+  it("pay for second order", async () => {
+    await expect(
+      marketplace.connect(receiver).buy(secondSessionId, {
+        value: BigNumber.from(10),
+      }),
+    )
+      .to.emit(marketplace, "OrderAccepted")
+      .withArgs(secondSessionId);
+
+    expect(await marketplace.provider.getBalance(marketplace.address)).equal(
+      15,
     );
+  });
+
+  it("reveal fake key for second order", async () => {
+    await expect(marketplace.connect(sender).reveal(secondSessionId, fakeKey))
+      .to.emit(marketplace, "OrderRevealed")
+      .withArgs(fakeKey, secondSessionId);
+  });
+
+  it("successful complain about root because of fake key", async () => {
+    const tx = await marketplace
+      .connect(receiver)
+      .complainAboutRoot(
+        secondSessionId,
+        complainRootData.correctLeaf,
+        complainRootData.correctProof,
+      );
 
     await tx.wait();
 
-    expect(await marketplace.provider.getBalance(seller.address)).equal(
-      sellerBalance.add(BigNumber.from(5)),
-    );
+    expect(await marketplace.provider.getBalance(marketplace.address)).equal(5);
+  });
+
+  it("failed withdraw as sender for successful trade because timeout still active", async () => {
+    await expect(marketplace.connect(sender).refund(firstSessionId)).to.be
+      .reverted;
+  });
+
+  it("successful withdraw as sender for successful trade", async () => {
+    await time.increase(3800);
+
+    const tx = await marketplace.connect(sender).refund(firstSessionId);
+
+    await tx.wait();
+
+    expect(await marketplace.provider.getBalance(marketplace.address)).equal(0);
   });
 });
