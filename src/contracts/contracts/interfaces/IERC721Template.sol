506f772e0682fd6ae6193399bbefd0fe9d88ae14
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-02-18 03:05:41 +0100

    refactor(contracts): add remaining functions to IERC721Template interface

--- src/contracts/contracts/interfaces/IERC721Template.sol
+++ src/contracts/contracts/interfaces/IERC721Template.sol
@@ -6,39 +6,17 @@ pragma solidity ^0.8.17;
 // @author Kevin Hertwig <kevin.hertwig@gmail.com>
 // @title IERC721Template
 
-import "./IERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-interface IERC721Template is IERC721A {
-  enum RolesType {
-    Manager,
-    DeployERC20,
-    UpdateMetadata,
-    Store
-  }
-
+interface IERC721Template is IERC721 {
   enum Kind {
     dataset,
     algorithm
   }
 
-  event MetadataCreated(
-    address indexed createdBy,
-    uint8 state,
-    string decryptorUrl,
-    bytes flags,
-    bytes data,
-    string metaDataDecryptorAddress,
-    uint256 timestamp,
-    uint256 blockNumber
-  );
-
   event MetadataUpdated(
     address indexed updatedBy,
-    uint8 state,
-    string decryptorUrl,
-    bytes flags,
-    bytes data,
-    string metaDataDecryptorAddress,
+    string metadataURI,
     uint256 timestamp,
     uint256 blockNumber
   );
@@ -51,54 +29,17 @@ interface IERC721Template is IERC721A {
     Kind _kind
   ) external returns (bool);
 
-  struct Roles {
-    bool manager;
-    bool deployERC20;
-    bool updateMetadata;
-    bool store;
-  }
+  function addManager(address _manager) external;
 
-  struct metaDataProof {
-    address validatorAddress;
-    uint8 v; // v of validator signed message
-    bytes32 r; // r of validator signed message
-    bytes32 s; // s of validator signed message
-  }
+  function removeManager(address _manager) external;
 
-  function getPermissions(address user) external view returns (Roles memory);
+  function setPrice(uint256 _price) external;
 
-  function setDataERC20(bytes32 _key, bytes calldata _value) external;
+  function updateMetadata(string memory _metadataURI) external;
 
-  function setMetaData(
-    uint8 _metaDataState,
-    string calldata _metaDataDecryptorUrl,
-    string calldata _metaDataDecryptorAddress,
-    bytes calldata flags,
-    bytes calldata data,
-    bytes32 _metaDataHash,
-    metaDataProof[] memory _metadataProofs
-  ) external;
+  function pause() external;
 
-  function getMetaData()
-    external
-    view
-    returns (string memory, string memory, uint8, bool);
+  function unpause() external;
 
-  function createERC20(
-    uint256 _templateIndex,
-    string[] calldata strings,
-    address[] calldata addresses,
-    uint256[] calldata uints,
-    bytes[] calldata bytess
-  ) external returns (address);
-
-  function removeFromCreateERC20List(address _allowedAddress) external;
-
-  function addToCreateERC20List(address _allowedAddress) external;
-
-  function addToMetadataList(address _allowedAddress) external;
-
-  function removeFromMetadataList(address _allowedAddress) external;
-
-  function getId() external pure returns (uint8);
+  function withdraw() external;
 }
