2c6e59b54d5932f365312e444cab089a9482e4fb
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-02-27 01:52:42 +0100

    chore(compute): add marketplace and sender private key to config

--- src/compute-node/src/config/index.ts
+++ src/compute-node/src/config/index.ts
@@ -1,4 +1,11 @@
 import { config } from "dotenv";
 config({ path: `../../.env` });
 
-export const { NODE_ENV, PORT, LOG_FORMAT, LOG_DIR } = process.env;
+export const {
+  NODE_ENV,
+  PORT,
+  LOG_FORMAT,
+  LOG_DIR,
+  MARKETPLACE_ADDRESS,
+  SENDER_PRIVATE_KEY,
+} = process.env;
