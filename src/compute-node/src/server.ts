3ae2b53577b39694ae2734593f414e2eab9e8f5a
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-03-11 23:10:37 +0100

    refactor(compute): remove unnecessary routes

--- src/compute-node/src/server.ts
+++ src/compute-node/src/server.ts
@@ -1,6 +1,5 @@
 import { startApp } from "@/app";
-import AssetsRouter from "@routes/assets.route";
 import SetupRouter from "@/routes/setup.route";
 import OrdersRouter from "@/routes/order.route";
 
-startApp([AssetsRouter(), SetupRouter(), OrdersRouter()]);
+startApp([SetupRouter(), OrdersRouter()]);
