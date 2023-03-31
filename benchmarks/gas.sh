45b5322424f7a27eca4829add06b09ebef432c84
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-03-31 23:48:23 +0200

    feat: gas benchmarks for different variants of verifiers

--- benchmarks/gas.sh
+++ benchmarks/gas.sh
@@ -6,41 +6,50 @@ declare -a proofs_iterative=("1" "2" "4" "8" "16")
 declare -a proofs_merkle=("2" "4")
 declare -a folders=("max" "mean" "median" "min" "standard_deviation" "sum" "threshold" "variance")
 
+export PATH=$PATH:~/.zokrates/bin
 
-# for folder in "${folders[@]}"
-# do
-#     RESULT="$DIR/$folder/iterative/$folder.txt"
-    
-#     if [ ! -f "$RESULT" ] 
-#     then
-#         echo "proof,timestamp,compiled_size,proving_key_size,verification_key_size,verifier_size" > $RESULT
-#     fi
-    
-#     for proof in "${proofs[@]}"
-#     do
-#         cd $DIR/$folder/iterative/$proof/zokrates
-#         echo "Benchmarking: $proof"
+for folder in "${folders[@]}"
+do
+   RESULT="$DIR/$folder/iterative/$folder.txt"
+   
+   if [ ! -f "$RESULT" ] 
+   then
+       echo "proof,timestamp,deployment_gas,compiled_size,proving_key_size,verification_key_size,verifier_size" > $RESULT
+   else
+       rm -f "$DIR/$folder/merkle/$folder.txt"
+       echo "proof,timestamp,deployment_gas,compiled_size,proving_key_size,verification_key_size,verifier_size" > $RESULT
+   fi
+   
+   for proof in "${proofs_iterative[@]}"
+   do
+       cd $DIR/$folder/iterative/$proof/zokrates
+       zokrates export-verifier
+       pwd
+       echo "Benchmarking: $proof"
 
-#         compiled_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/out | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
-#         proving_key_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/proving.key  | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
-#         verification_key_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/verification.key | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
-#         verifier_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/verifier.sol | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
+       compiled_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/out | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
+       proving_key_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/proving.key  | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
+       verification_key_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/verification.key | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
+       verifier_size=$(du -kh $DIR/$folder/iterative/$proof/zokrates/verifier.sol | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
 
-#         cd ..
-#         cd ./verification
+       cp ./verifier.sol ../../../../contracts
+       cp ./proof.json ../../../../contracts
 
-#         #deployment_gas=$(truffle test | cut -f1 | grep "Verifier" | awk '{print $8}' | xargs)
+       cd $DIR
+       pwd
+       deployment_gas=$(yarn test | cut -f1 | grep "Verifier" | awk '{print $8}' | xargs)
 
-#         row="$proof,$(date +%s),$compiled_size,$proving_key_size,$verification_key_size,$verifier_size"
+       row="$proof,$(date +%s),$deployment_gas,$compiled_size,$proving_key_size,$verification_key_size,$verifier_size"
 
-#         echo $row >> $RESULT
-#     done
-# done
+       echo $row >> $RESULT
+   done
+   sleep 5
+done
 
 for folder in "${folders[@]}"
 do
     RESULT="$DIR/$folder/merkle/$folder.txt"
-    
+
     if [ ! -f "$RESULT" ] 
     then
         echo "proof,timestamp,deployment_gas,compiled_size,proving_key_size,verification_key_size,verifier_size" > $RESULT
@@ -48,21 +57,24 @@ do
         rm -f "$DIR/$folder/merkle/$folder.txt"
         echo "proof,timestamp,deployment_gas,compiled_size,proving_key_size,verification_key_size,verifier_size" > $RESULT
     fi
-    
+
     for proof in "${proofs_merkle[@]}"
     do
         cd $DIR/$folder/merkle/$proof/zokrates
+        zokrates export-verifier
+        pwd
         echo "Benchmarking: $proof"
 
         compiled_size=$(du -kh $DIR/$folder/merkle/$proof/zokrates/out | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
         proving_key_size=$(du -kh $DIR/$folder/merkle/$proof/zokrates/proving.key  | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
         verification_key_size=$(du -kh $DIR/$folder/merkle/$proof/zokrates/verification.key | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
         verifier_size=$(du -kh $DIR/$folder/merkle/$proof/zokrates/verifier.sol | cut -f1 | sed 's/\([0-9]\),/\1./g' | xargs)
-
+        
         cp ./verifier.sol ../../../../contracts
         cp ./proof.json ../../../../contracts
 
         cd $DIR
+        pwd
         deployment_gas=$(yarn test | cut -f1 | grep "Verifier" | awk '{print $8}' | xargs)
 
         row="$proof,$(date +%s),$deployment_gas,$compiled_size,$proving_key_size,$verification_key_size,$verifier_size"
